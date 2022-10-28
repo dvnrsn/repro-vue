3df5a875a608bdabb15b76e571148b548a838bad
Devin Rasmussen <drasmussen@sleuth.io>
2022-10-28 12:16:07 +0100

    only-commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import type SimpleName from "./SimpleName.vue";
+
+
+type Burrito = {
+  __type__: "burrito";
+};
+
+type Salad = {
+  __type__: "salad";
+  dressing: string;
+};
+
+const props = defineProps<{
+  food: Salad | Burrito;
+}>();
+
+const isSalad = computed(() => props.food.__type__ === "salad");
+</script>
+
+<template>
+  <SimpleName :message="isSalad ? props.food.dressing : 'none'" />
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/SimpleName.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="greetings">{{ props.msg }}</div>
+</template>
+
+<style scoped>
+
+</style>
